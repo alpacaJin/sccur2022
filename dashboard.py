2ae9fa135752ff4f1f9c5437f72694536b2db65e
Sandra Nguyen <67350265+alpacaJin@users.noreply.github.com>
2022-11-18 23:54:27 -0800

    changes to imports

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-import plotly.express as px
 from streamlit_tags import st_tags
-import requests, lxml, json, time, tldextract
+import requests, json, tldextract
 from bs4 import BeautifulSoup
 from csv import writer
 
